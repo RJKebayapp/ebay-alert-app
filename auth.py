2205b8b1616cbc868615b07ada418f61359c782e
R.J. Kesting <r.j.kesting@RJs-MacBook-Air.local>
2025-03-27 19:25:08 +0100

    Fix: updated import statements to absolute imports

--- auth.py
+++ auth.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import Session
 from .models import User
 from .utils import hash_password, generate_jwt_token, get_db
 from .email_utils import send_registration_email
-from auth import router as auth_router
+from .models import User
+from .utils import hash_password, generate_jwt_token, get_db
+from .email_utils import send_registration_email
 
 
 router = APIRouter()
